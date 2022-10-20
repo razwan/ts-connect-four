50330df1ab4915188a4975240defa924fa996160
ostanmadalina92 <ostan.madalina@yahoo.com>
2022-10-20 12:27:32 +0300

    style emoji

--- src/components/ScoreCardEmoji/ScoreCardEmoji.style.tsx
+++ src/components/ScoreCardEmoji/ScoreCardEmoji.style.tsx
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 
 export const ScoreCardEmojiImageLeft = styled.img`
-  margin-right: -28px;
-  z-index: 1;
+  position: absolute;
+  top: 50%;
+  left: -3.5rem;
+  translate: 50% -50%;
 `;
 
 export const ScoreCardEmojiImageRight = styled.img`
-  margin-right: -200px;
-  z-index: 1;
+  position: absolute;
+  top: 50%;
+  right: 0rem;
+  translate: 50% -50%;
 `;
