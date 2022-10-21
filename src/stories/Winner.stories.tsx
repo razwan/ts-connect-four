aabbe4ec4892de479ea269dd6ed70c454f94f30a
ostanmadalina92 <ostan.madalina@yahoo.com>
2022-10-21 11:30:02 +0300

    add winner

---
+++ src/stories/Winner.stories.tsx
@@ -0,0 +1,12 @@
+import { Winner } from "../components";
+import { Story } from "@storybook/react";
+
+export default {
+  title: "Winner",
+  component: Winner,
+};
+
+const Template: Story = (args: any) => <Winner {...args} />;
+
+export const WinnerStory = Template.bind({});
+WinnerStory.args = {};
