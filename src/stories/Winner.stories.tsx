9ee9a740afb9e7f24d5b91f96cd7833f88297202
ostanmadalina92 <ostan.madalina@yahoo.com>
2022-10-24 10:59:10 +0300

    theme provider added

--- src/stories/Winner.stories.tsx
+++ src/stories/Winner.stories.tsx
@@ -1,12 +1,20 @@
 import { Winner } from "../components";
 import { Story } from "@storybook/react";
+import { IButton } from "../components/Button/Button";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../styles/Theme.style";
+
 
 export default {
   title: "Winner",
   component: Winner,
 };
 
-const Template: Story = (args: any) => <Winner {...args} />;
+const Template: Story = (args: any) => (
+  <ThemeProvider theme={theme}>
+    <Winner {...args} />
+  </ThemeProvider>
+);
 
 export const WinnerStory = Template.bind({});
 
