71b4dc8210c167f8cbcc56f45068c5428b60a854
Razvan Onofrei <razwan.me@gmail.com>
2022-10-27 16:05:13 +0300

    Fix timer story

--- src/stories/Timer.stories.tsx
+++ src/stories/Timer.stories.tsx
@@ -1,12 +1,19 @@
 import { Timer } from "../components";
 import { Story } from "@storybook/react";
+import AppContext, { withContextProvider } from '../AppContext';
 
 export default {
    title: "Timer",
    component: Timer,
 }
 
-const Template: Story = (args: any) => <Timer {...args}/>
+const TimerWithContext = withContextProvider( ( props: any ) => {
+   return (
+      <Timer { ...props } />
+   )
+} );
+
+const Template: Story = (args: any) => <TimerWithContext {...args}/>
 
 export const TimerStory =  Template.bind({});
 TimerStory.args = {
