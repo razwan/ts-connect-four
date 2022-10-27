5d99fe672fc9ceec27534abf62a9d863e92c9084
BiancaVeres <biancaveres93@gmail.com>
2022-10-27 14:08:23 -0700

    added colors to disc story

--- src/stories/Disc.stories.tsx
+++ src/stories/Disc.stories.tsx
@@ -11,10 +11,12 @@ const Template: Story<IDisc> = (args: any) => <Disc {...args} />;
 
 export const DiscPlayer1 = Template.bind({});
 DiscPlayer1.args = {
+    color: '#FD6687',
     winner: false,
 };
 
 export const DiscPlayer2 = Template.bind({});
 DiscPlayer2.args = {
+    color: '#FFCE67',
     winner: true,
 };
