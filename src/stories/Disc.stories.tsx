42e23d78ef56fa605df2e4ac5c24aff55544477f
BiancaVeres <biancaveres93@gmail.com>
2022-10-27 14:21:00 -0700

    added color variables

--- src/stories/Disc.stories.tsx
+++ src/stories/Disc.stories.tsx
@@ -1,22 +1,23 @@
 import Disc from "../components/Disc";
 import { IDisc } from "../components/Disc/Disc";
 import { Story } from "@storybook/react";
+import { ThemeProvider } from 'styled-components';
+import { theme } from "../styles/Theme.style";
 
 export default {
     title: 'Disc',
     component: Disc,
 }
-
-const Template: Story<IDisc> = (args: any) => <Disc {...args} />;
+const Template: Story<IDisc> = (args: any) => <ThemeProvider theme={theme}><Disc {...args} /></ThemeProvider>;
 
 export const DiscPlayer1 = Template.bind({});
 DiscPlayer1.args = {
-    color: '#FD6687',
+    color: theme.colors.pink,
     winner: false,
 };
 
 export const DiscPlayer2 = Template.bind({});
 DiscPlayer2.args = {
-    color: '#FFCE67',
+    color: theme.colors.yellow,
     winner: true,
 };
